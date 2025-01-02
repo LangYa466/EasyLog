cb3168aff3c4e5c4e1c4124ada387dd72b0e306c
LangYa466 <langya466@gmail.com>
2025-01-02 21:00:31 +0800

    不带它玩

--- src/test/java/com/langya/LogPerformanceTest.java
+++ src/test/java/com/langya/LogPerformanceTest.java
@@ -1,15 +1,10 @@
 package com.langya;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 /**
  * @author LangYa
  * @since 2025/1/2
  */
 public class LogPerformanceTest {
-    private static final Logger log4jLogger = LogManager.getLogger(LogPerformanceTest.class);
-
     public static void main(String[] args) {
         int logCount = 1000000;
 
@@ -20,13 +15,6 @@ public class LogPerformanceTest {
         }
         long endTimeEasyLog = System.nanoTime();
 
-        // Measure log4j performance
-        long startTimeLog4j = System.nanoTime();
-        for (int i = 0; i < logCount; i++) {
-            log4jLogger.info("Test message {}", i);
-        }
-        long endTimeLog4j = System.nanoTime();
-
         // Measure TinyLog performance
         long startTimeTinyLog = System.nanoTime();
         for (int i = 0; i < logCount; i++) {
@@ -35,7 +23,6 @@ public class LogPerformanceTest {
         long endTimeTinyLog = System.nanoTime();
 
         System.out.println("EasyLog Time: " + (endTimeEasyLog - startTimeEasyLog) / 1_000_000 + " ms");
-        System.out.println("Log4j Time: " + (endTimeLog4j - startTimeLog4j) / 1_000_000 + " ms");
         System.out.println("TinyLog Time: " + (endTimeTinyLog - startTimeTinyLog) / 1_000_000 + " ms");
         System.exit(0);
     }
